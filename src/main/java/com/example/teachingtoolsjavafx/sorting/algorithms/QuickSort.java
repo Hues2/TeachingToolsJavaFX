68563a02ec33bfa0ef59af69420d6ac8607d8429
Greg Ross <gregandrewross79@gmail.com>
2022-04-08 01:43:16 +0100

    Added definition and pseudocode to quicksort

--- src/main/java/com/example/teachingtoolsjavafx/sorting/algorithms/QuickSort.java
+++ src/main/java/com/example/teachingtoolsjavafx/sorting/algorithms/QuickSort.java
@@ -31,7 +31,7 @@ public class QuickSort extends SortingAlgorithm{
                 list = new Bar[randomBars.length];
                 System.arraycopy(randomBars, 0, list, 0, list.length);
                 listOfLists.add(list);
-            } 
+            }
         }
 
         swap(i + 1, high);
@@ -54,6 +54,26 @@ public class QuickSort extends SortingAlgorithm{
 
     @Override
     public String definitionText() {
-        return "Testing quicksort";
+        return """
+                 The Quick Sort is one of the most efficient sorting algorithms and is based on the splitting of an array (partition) into smaller ones and swapping (exchange) based on the comparison with 'pivot' element selected.
+
+
+
+                PSEUDO CODE:
+
+                QuickSort(arr[], low, high)
+                 {
+                     if (low < high)
+                     {
+                         // pivot_index is partitioning index, arr[pivot_index] is now at correct place in sorted array
+                         pivot_index = partition(arr, low, high);
+                 
+                         quickSort(arr, low, pivot_index - 1);  // Before pivot_index
+                         quickSort(arr, pivot_index + 1, high); // After pivot_index
+                     }
+                 }
+                 
+                 Definition and pseudocode provided by InterviewBit, https://www.interviewbit.com/tutorial/quicksort-algorithm/
+                 """;
     }
 }
