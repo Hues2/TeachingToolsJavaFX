0f46e578e7a7c6808d77075d246bec26207675a9
Greg Ross <gregandrewross79@gmail.com>
2022-04-12 15:35:23 +0100

    Added decimal format

--- src/main/java/com/example/teachingtoolsjavafx/sorting/algorithms/AlgorithmTimer.java
+++ src/main/java/com/example/teachingtoolsjavafx/sorting/algorithms/AlgorithmTimer.java
@@ -1,5 +1,7 @@
 package com.example.teachingtoolsjavafx.sorting.algorithms;
 
+import java.text.DecimalFormat;
+
 public class AlgorithmTimer {
     private double startTime;
     private double endTime;
@@ -13,6 +15,8 @@ public class AlgorithmTimer {
     }
 
     public String getTotalTimeInMS(){
-        return (((endTime - startTime) / 1000000) + "ms");
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        return (df.format((endTime - startTime) / 1000000) + "ms");
     }
 }
